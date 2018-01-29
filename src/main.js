f14670e8910481c31b57a44e6e4edad44ef5aa81
Chris Zhan <chris.zhan@ringcentral.com>
2018-01-29 16:36:07 +0800

    Do not expose GenericDao

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import DbManager from './lib/base/dbManager';
-import GenericDao from './lib/base/genericDao';
-
 import GroupDao from './lib/impls/groupDao';
 
-export { DbManager, GenericDao, GroupDao };
+export { DbManager, GroupDao };
